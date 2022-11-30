1c39df0307764b60f405e568bb01afbf29e5bfcf
BaghdadTheBrave <storchunv@ukr.net>
2022-11-30 11:35:55 +0200

    trying to listen port pt2

--- BBE-1/Program.cs
+++ BBE-1/Program.cs
@@ -3,7 +3,8 @@ using BBE_1.Services.Data;
 var builder = WebApplication.CreateBuilder(args);{
     builder.Services.AddHttpsRedirection(options =>
 {
-    options.HttpsPort = int.TryParse(Environment.GetEnvironmentVariable("PORT"), out var p) ? p : null;
+    Console.WriteLine(int.TryParse(Environment.GetEnvironmentVariable("PORT"), out var p) ? p : null);
+    options.HttpsPort = int.TryParse(Environment.GetEnvironmentVariable("PORT"), out var p2) ? p2 : null;
 });
     builder.Services.AddControllers();
     builder.Services.AddSingleton<IUserService,UserService>();
